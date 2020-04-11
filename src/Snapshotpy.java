e0e4d0ea4f113791cffa85f0f981d33ff951ed77
Daniel <cndanielwang@gmail.com>
2020-04-11 17:52:52 -0400

    clean up Snapshotpy

--- src/Snapshotpy.java
+++ src/Snapshotpy.java
@@ -3,14 +3,8 @@ import java.io.File;
 
 public class Snapshotpy implements Runnable
 {
-    BufferedImage bufferedImg;
-
-    // To receive content that will be outputted
-    String str = "";
-    int imgWidth, imgHeight, imgResolution;
-    double x = 0;
-    String imgPath = CharGrapher.ssgWS + "SSGSHOTS_IMG.jpg";
-    File imgfile = new File(imgPath);
+    private String imgPath = CharGrapher.ssgWS + "SSGSHOTS_IMG.jpg";
+    private File imgfile = new File(imgPath);
 
     public void run()
     {
@@ -32,7 +26,7 @@ public class Snapshotpy implements Runnable
         if(pic.resolution > CharGrapher.maxResolution)
         {
             // Get the ratio to compress
-            x = CharGrapher.maxResolution / pic.resolution;
+            double x = CharGrapher.maxResolution / pic.resolution;
             x = Math.sqrt(x);
             // The 0.8 and 1.1 is ratio that adjust the output to suit the font.
             pic.compress((int) (pic.height * x * 0.8), (int) (pic.width * x * 1.1));
